ba8ad7b216b019a1ee7bb44036644e4d948eaddc
ayasko <alena_yasko@epam.com>
2020-06-29 11:52:06 +0300

    Register congfig server with Eureka

--- src/main/java/com/epam/yoke/config/YokeConfigServer.java
+++ src/main/java/com/epam/yoke/config/YokeConfigServer.java
@@ -3,9 +3,11 @@ package com.epam.yoke.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class YokeConfigServer {
 
   public static void main(String[] args) {
